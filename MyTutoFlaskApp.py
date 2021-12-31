2459304c95949c57ee6ad21971096937702178a2
Bhargav <bhargavkadali39@gmail.com>
2021-12-31 16:34:46 +0530

    Add files via upload

---
+++ MyTutoFlaskApp.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template,request
+import pyshorteners as sh
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    url = "this is live"
+    return render_template('index.html',content=url)
+
+@app.route("/result", methods=['get','post'])
+def result():
+    output = request.form.to_dict()
+    name = output["name"]
+    link = name
+    s = sh.Shortener()
+    x = (s.tinyurl.short(link))
+    return render_template('index.html',tinyy=x)
+if __name__ == '__main__':
+    app.run()
+
+
+
+
+
+
